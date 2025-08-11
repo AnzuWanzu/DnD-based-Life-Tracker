e62157cedd873ec8858dd4f4daa958a9e6300674
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-11 21:08:53 +0800

    basic chat completion done, scenario test for OC

--- groqChat_v1.js
+++ groqChat_v1.js
@@ -2,18 +2,22 @@ import Groq from "groq-sdk";
 import 'dotenv/config';
 const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
 
-async function main(){
+export async function main(){
     const chatCompletion = await getGroqChatCompletion();
     console.log(chatCompletion.choices[0]?.message?.content || ""
     );
 }
 
-async function getGroqChatCompletion(){
+export async function getGroqChatCompletion(){
     return groq.chat.completions.create({
         messages: [
             {
+            role: "system",
+            content: "You are a well-known DM named Matt Mercer. Giving scenarios about Rath Zalothar's (a Red Dragonborn Fighter) current scenario asking questions like: You encountered..., what do you do?",
+            },
+            {
             role: "user",
-            content: "Can you give me a short lore for a Red Dragonborn Fighter named Rath Zalothar.",
+            content: "DM, what is it I'm facing?",
             },
     ],
     model: "llama-3.3-70b-versatile",
